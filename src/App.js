83dfe0307892979b0c0b3f0417ac68650472e8a2
IamHaque <iamhaque2@gmail.com>
2021-05-09 23:35:59 +0530

    Fixed auth issue

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import "./App.css";
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_API_KEY,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
